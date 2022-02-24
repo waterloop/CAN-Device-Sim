5f5f8d6ed4fb4833ef4f005a7e88a08bba3ce5dc
Quinn1876 <qhodges0442@gmail.com>
2022-02-24 01:44:35 +0000

    Added Command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use virtual_can_device::run_devices;
 use virtual_can_device::get_configs;
 use text_io::scan;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long, default_value = "vcan0")]
+    can_interface: String,
+    #[clap(long, default_value = "./configs")]
+    config_folder: String,
+}
 fn main() {
-    let configs = get_configs().unwrap();
+    let args = Args::parse();
+    let configs = get_configs(&args.config_folder).unwrap();
     println!("Please Choose a Config:");
     for (i, config) in configs.iter().enumerate() {
         println!("{}. {}", i, config);
@@ -13,7 +23,9 @@ fn main() {
     scan!("{}", input);
     let filename = configs.get(input).unwrap();
     let config =  virtual_can_device::config::Config::from(&filename);
-    let dm = run_devices(&config, "vcan0");
+    let mem_leaker = Box::new(args.can_interface);
+    let can_interface: &'static str = Box::leak(mem_leaker);
+    let dm = run_devices(&config, can_interface);
     let input2: String;
     scan!("{}", input2);
     dm.stop_devices();
